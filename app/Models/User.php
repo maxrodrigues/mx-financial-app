08558e835a88e2c03edb2e20f4d4db800967bad0
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-09-09 18:30:03 -0300

    FA-12: install and configure larastan

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /**
+     * @use HasFactory<UserFactory>
+     */
     use HasFactory, Notifiable;
 
     /**
