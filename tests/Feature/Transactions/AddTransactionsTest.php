a8e3b0e1bb1ef21ebe212b63fae940735bdadb67
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-09-17 10:29:49 -0300

    FA-14: should transaction must belong to a wallet

--- tests/Feature/Transactions/AddTransactionsTest.php
+++ tests/Feature/Transactions/AddTransactionsTest.php
@@ -1,14 +1,18 @@
 <?php
 
-use App\Models\User;
+use App\Models\{User, Wallet};
 
 use function Pest\Laravel\{actingAs, assertDatabaseCount};
 
 it('should be add new transaction', function () {
     $user = User::factory()->create();
     actingAs($user);
+    $wallet = Wallet::factory()
+        ->for($user)
+        ->create();
 
     $response = $this->post(route('transactions.store'), [
+        'wallet_id'      => $wallet->id,
         'type'           => 'credit',
         'amount'         => 1000,
         'transaction_at' => '2024-10-10',
@@ -32,4 +36,36 @@ it('should be validate attributes to store a transaction', function () {
         'transaction_at',
         'description',
     ]);
+});
+
+it('should transaction must belong to a wallet', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+    $wallet = Wallet::factory()
+        ->for($user)
+        ->create();
+
+    $this->post(route('transactions.store'), [
+        'type'           => 'credit',
+        'amount'         => 1000,
+        'transaction_at' => '2024-10-10',
+        'description'    => 'Test transaction',
+        'observation'    => 'Test observation',
+    ])
+        ->assertSessionHasErrors([
+            'wallet_id',
+        ]);
+
+    assertDatabaseCount('transactions', 0);
+
+    $this->post(route('transactions.store'), [
+        'wallet_id'      => $wallet->id,
+        'type'           => 'credit',
+        'amount'         => 1000,
+        'transaction_at' => '2024-10-10',
+        'description'    => 'Test transaction',
+        'observation'    => 'Test observation',
+    ]);
+
+    assertDatabaseCount('transactions', 1);
 });
